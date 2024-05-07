1531d888faa81143e0722357feeca72681e41937
Ali Imran <91894614+aliimran483@users.noreply.github.com>
2024-05-07 14:49:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+<a href="https:github.com/aliimran483/Apple-website" onclick="window.open('https:github.com/aliimran483/Apple-website', '_self');">
+Example
+
+
+</a>
